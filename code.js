a3edec8673806e617d98d37c9c988845abf98338
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-05-07 18:43:15 +0000

    update 11

--- code.js
+++ code.js
@@ -1,28 +1,27 @@
 function depthFirstSearch(graph, startNode, targetNode) {
     let path = [];
-    let visit = [];
-    if(graph.length === 0 || DFS(graph, startNode, targetNode, visit, path) === false){
+    let visit = new Array(graph.length).fill(false);
+    if (graph.length === 0 || DFS(graph, startNode, targetNode, visit, path) === false) {
         return [];
-    }
-    else{
+    } else {
         return path;
-    }    
+    }
 }
 
-function DFS(graph, start, target, visit, path){
-    if(start == target){
-        visit.push(start);
+function DFS(graph, start, target, visit, path) {
+    if (start == target) {
+        visit[start] = true;
         path.push(start);
         return true;
     }
 
-    visit.push(start);
+    visit[start] = true;
     path.push(start);
 
-    for(let i = 0; i < graph[start].length; i++){
+    for (let i = 0; i < graph[start].length; i++) {
         let current = graph[start][i];
-        if(visit.indexOf(current) === -1){
-            if(DFS(graph, current, target, visit, path)){
+        if (!visit[current]) {
+            if (DFS(graph, current, target, visit, path)) {
                 return true;
             }
         }
